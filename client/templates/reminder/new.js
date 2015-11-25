cafbca7d2e8d222bdffc09e6617812403ba93597
Gardezi1 <ali.gardezi@kwanso.com>
2015-11-25 21:24:38 +0500

    dynamic cron job done

--- client/templates/reminder/new.js
+++ client/templates/reminder/new.js
@@ -5,20 +5,9 @@ var hooksObject = {
 	after: {
     // Replace `formType` with the form `type` attribute to which this hook applies
 	    insert: function(error, result) {
-	    	debugger;
-	    	console.log(result);
-	    	debugger;	
-	    	if (ServerSession.get('currentId') == undefined){
-		    	ServerSession.set('currentId', result);
-		    }else{
-		    	 var oldReminder = Reminders.findOne({_id: ServerSession.get('currentId')});
-		    	 var newReminder = Reminders.findOne({_id: result});
-		    	 if(oldReminder.date > newReminder.date){
-		    	 	ServerSession.set('currentId', result);
-		    	 }else if (oldReminder.time > newReminder.time){
-		    	 	ServerSession.set('currentId', result);
-		    	 }
-		    }
+		    Meteor.call("intiateReminder" , result , function(error , result){
+		    	console.log("Hello World")
+		    })
 	    	
 	    }
   	}
--- server/method.js
+++ server/method.js
@@ -1,23 +1,22 @@
-var recurrence = new Cron(everyMinute , {});
 
-var everyMinute = new Cron(function() {
-    console.log("another minute has passed!");
-    var _date = (new Date).toLocaleString().split(",");
-    console.log(_date);
-    var p = Reminders.findOne({_id: ServerSession.get('currentId')}).date.toLocaleString().split(",")[0];
-    if (date == p){
-    	p = Reminders.findOne({_id: ServerSession.get('currentId')}).time.toLocaleString();
-    	tt = (new Date).getHours().toLocaleString();
-    	s = (new Date).getMinutes();
-    	if (s/10 == 0 ){
-    		tt = tt + ":0" + s;
-    	}else{
-    		tt = tt+":"+s;
-    	}
+Meteor.methods({
+    intiateReminder:function(data){
+        console.log(data);
+        var d = new Date(Reminders.findOne({_id: data}).date.getFullYear(), Reminders.findOne({_id: data}).date.getMonth() + 1, Reminders.findOne({_id: data}).date.getDate(), parseInt(Reminders.findOne({_id: data}).time.split(":")[0]), parseInt(Reminders.findOne({_id: data}).time.split(":")[1]), 0, 0);
+        console.log("Hello World");
+        console.log(d);
+        //  var newReminder = Reminders.findOne({_id: data});
+         var recurrence = new Cron(everyMinute , {hour: d.getHours() , minute: d.getMinutes()});
 
-    	if (p ==  tt){
-    		Router.go('alertPage')
-    	}
+         var everyMinute = new Cron(function() {
+            console.log("Outputting Data");
+            console.log(data);
+        }, {
+            minute: d.getMinutes(),
+            hour: d.getHours(),
+          day: d.getDate(),
+            month: d.getMonth()+1
+        });
 
     }
-}, {});
+});
